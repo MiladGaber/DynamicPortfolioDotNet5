016fec0aee961b1b7045a1408e425311fd6e0137
Milad Gaber <milad.gaber00@gmail.com>
2021-12-19 23:00:36 +0200

    Adding Models In Core Entity Base, Owner ,Adress, Portfolio

---
+++ Core/Entities/Owner.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Entities
+{
+    public class Owner : EntityBase
+    {
+        public string FullName { get; set; }
+        public string JopTitle { get; set; }
+        public string AvatarUrl { get; set; }
+
+        public Address Address{ get; set; }
+    }
+}
